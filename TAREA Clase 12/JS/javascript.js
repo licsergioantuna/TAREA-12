38b60d790f50d868f63b336f5912a745eeda99e2
licsergioantuna <124640783+licsergioantuna@users.noreply.github.com>
2023-03-06 00:00:10 -0300

    Add files via upload

---
+++ TAREA Clase 12/JS/javascript.js
@@ -0,0 +1,14 @@
+let miNombre = prompt ("Ingresa tu nombre");
+console.log(miNombre);
+
+
+if(miNombre){
+            alert("Bienvenido " + miNombre);
+            }
+
+else{
+    alert("Por favor debe ingresar su nombre para continuar");
+}
+
+
+  
